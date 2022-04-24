4e41ae35bb1e42322da3556b1a41f6bb065497c2
fatimaosman99 <54881403+fatimaosman99@users.noreply.github.com>
2022-04-24 08:47:37 +0200

    Added recording of easter program

--- Påske/runner.py
+++ Påske/runner.py
@@ -1,5 +1,6 @@
 import datetime
 def isItEaster2023():
+    print("This program tells when it is easter. It checks today's date and sees if it's easter. It only checks 2023's easter though.")
     print("Is it easter?")
     if datetime.datetime.now() == datetime.datetime(2023, 4, 9):
         print("it is easter.")
